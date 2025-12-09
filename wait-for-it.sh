107b0b12a49a07a10f99113a0848ddf0b851c51b
Max Base <MaxBaseCode@Gmail.Com>
2025-12-10 02:42:42 +0330

    Update wait-for-it.sh

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,1 +1,28 @@
-CMD sh -c 'until nc -z meilisearch 7700; do echo "Waiting for Meilisearch..."; sleep 1; done; ./scratch-meilisearch -h meilisearch -p 7700'
+#!/bin/sh
+
+set -e
+
+HOSTPORT="$1"
+shift
+
+TIMEOUT=30
+START=$(date +%s)
+
+HOST=$(echo $HOSTPORT | cut -d':' -f1)
+PORT=$(echo $HOSTPORT | cut -d':' -f2)
+
+echo "Waiting for $HOST:$PORT..."
+
+while ! nc -z "$HOST" "$PORT"; do
+  NOW=$(date +%s)
+  ELAPSED=$((NOW - START))
+  if [ "$ELAPSED" -ge "$TIMEOUT" ]; then
+    echo "Timeout reached ($TIMEOUT s), host not available."
+    exit 1
+  fi
+  sleep 1
+done
+
+echo "$HOST:$PORT is available!"
+
+exec "$@"
